4a58f0d5b069f8b219d5c3fe503f193ae103810c
Aidan O'Brien <83436178+aidanwobrien@users.noreply.github.com>
2022-04-26 15:58:56 -0400

    Add files via upload

---
+++ ebirdORM.py
@@ -0,0 +1,15 @@
+from ebird.api import get_observations
+import pandas as pd
+from datetime import datetime
+
+
+today = datetime.today().strftime('%Y-%m-%d')
+
+api_key = 'uh892sarp2s8'
+
+def job():
+    records = get_observations(api_key, 'CA-ON', back = 30)
+    df = pd.DataFrame(records)
+    df.to_csv('C:/Users/Owner/Desktop/Python Scripts/outputs/sar_observations' + today + '.csv')
+
+print('done!')
